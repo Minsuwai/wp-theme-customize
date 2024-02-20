6cfc6bec3292ceaa23f369c6c42f7ecbd6bd5119
minsuwai <mswwai15@gmail.com>
2024-02-20 21:28:18 +0630

    update2021

--- wp-content/themes/aquila/template-parts/components/blog/entry-content.php
+++ wp-content/themes/aquila/template-parts/components/blog/entry-content.php
@@ -12,22 +12,32 @@
 
 <div class="entry-content">
     <?php
-        if ( is_single() ) {
-            the_content(
-                sprintf(
-                    wp_kses(
-                        __('Continue reading %s <span class="meta-nav">&rarr;</span>', 'aquila'),
-                        [
-                            'span' =>[
-                                'class' => []
-                            ]
+    if (is_single()) {
+        the_content(
+            sprintf(
+                wp_kses(
+                    __('Continue reading %s <span class="meta-nav">&rarr;</span>', 'aquila'),
+                    [
+                        'span' => [
+                            'class' => []
                         ]
-                            ),
-                            the_title( '<span class="screen-reader-text">"', '"</span>', false )
-                )
-                    );
-                } else {
-                    aquila_the_excerpt();
-                }
+                    ]
+                ),
+                the_title('<span class="screen-reader-text">"', '"</span>', false)
+            )
+        );
+
+        wp_link_pages(
+            [
+                'before' => '<div class="page-links">' . esc_html__('Pages:', 'aquila'),
+                'after' => '</div>',
+            ]
+        );
+    } else {
+        aquila_the_excerpt();
+        echo aquila_excerpt_more();
+    }
+
+
     ?>
 </div>
