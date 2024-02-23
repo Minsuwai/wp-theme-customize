04ca4f8d51c3f47ef05e84803ef1324cc2cab683
minsuwai <mswwai15@gmail.com>
2024-02-23 16:08:44 +0630

    update223

--- wp-content/themes/aquila/inc/classes/class-assets.php
+++ wp-content/themes/aquila/inc/classes/class-assets.php
@@ -1,12 +1,11 @@
 <?php
-
-/*
+/**
  * Enqueue theme assets
  *
  * @package Aquila
  */
 
-namespace Aquila_Theme\Inc;
+namespace AQUILA_THEME\Inc;
 
 use AQUILA_THEME\Inc\Traits\Singleton;
 
@@ -15,38 +14,37 @@ class Assets {
 
 	protected function __construct() {
 
-		//load class
+		// load class.
 		$this->setup_hooks();
 	}
 
 	protected function setup_hooks() {
 
-		/*
+		/**
 		 * Actions.
 		 */
-		add_action('wp_enqueue_scripts', [$this, 'register_styles']);
-		add_action('wp_enqueue_scripts', [$this, 'register_scripts']);
-
+		add_action( 'wp_enqueue_scripts', [ $this, 'register_styles' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'register_scripts' ] );
 	}
 
 	public function register_styles() {
-		// Register Styles
-		wp_register_style('style-css', get_stylesheet_uri(), [], filemtime( AQUILA_DIR_PATH . '/style.css'), 'all' );
-		wp_register_style('bootstrap-css', AQUILA_DIR_URI . '/assets/src/library/css/bootstrap.min.css', [], false, 'all' );
+		// Register styles.
+		wp_register_style( 'bootstrap-css', AQUILA_BUILD_LIB_URI . '/css/bootstrap.min.css', [], false, 'all' );
+		wp_register_style( 'main-css', AQUILA_BUILD_CSS_URI . '/main.css', ['bootstrap-css'], filemtime( AQUILA_BUILD_CSS_DIR_PATH . '/main.css' ), 'all' );
 
-		// Enqueue Styles
-		wp_enqueue_style('style-css');
-		wp_enqueue_style('bootstrap-css');
+		// Enqueue Styles.
+		wp_enqueue_style( 'bootstrap-css' );
+		wp_enqueue_style( 'main-css' );
 	}
 
 	public function register_scripts() {
-		// Register Scripts
-		wp_register_script('main-js', AQUILA_DIR_URI . '/assets/main.js', [], filemtime( get_template_directory() . '/assets/main.js'), true);
-		wp_register_script('bootstrap-js', AQUILA_DIR_URI . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true);
+		// Register scripts.
+		wp_register_script( 'main-js', AQUILA_BUILD_JS_URI . '/main.js', ['jquery'], filemtime( AQUILA_BUILD_JS_DIR_PATH . '/main.js' ), true );
+		wp_register_script( 'bootstrap-js', AQUILA_BUILD_LIB_URI . '/js/bootstrap.min.js', ['jquery'], false, true );
 
-
-		// Enqueue Scripts
-		wp_enqueue_script('main-js');
-		wp_enqueue_script('bootstrap-js');
+		// Enqueue Scripts.
+		wp_enqueue_script( 'main-js' );
+		wp_enqueue_script( 'bootstrap-js' );
 	}
+
 }
